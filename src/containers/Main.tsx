0e96b1d5bae4e7a53b030ea00d73e4c72cae5782
mason <mason.yk3@gmail.com>
2021-09-23 22:33:46 +0900

    feat: getRepo api 요청

--- src/containers/Main.tsx
+++ src/containers/Main.tsx
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import { Button } from "components/Button";
 import { Input } from "components/Input";
+import axios from "axios";
+
+const getRepo = (username: string) =>
+  axios.get(`https://api.github.com/users/${username}/repos`);
 
 function Main() {
+  const [value, setValue] = useState("");
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value);
+  };
+
+  const handleClick = () => {
+    getRepo(value).then(console.log);
+  };
+
+  const handleKeyUp = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      getRepo(value).then(console.log);
+    }
+  };
+
   return (
     <Container>
       <Header>
@@ -12,8 +32,8 @@ function Main() {
           Unofficial GitHub Star ranking for users, organizations and
           repositories
         </Description>
-        <Input />
-        <StyledButton>Search</StyledButton>
+        <Input onChange={handleChange} value={value} onKeyUp={handleKeyUp} />
+        <StyledButton onClick={handleClick}>Search</StyledButton>
       </Header>
     </Container>
   );
